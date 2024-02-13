b466cb3dac06f56798b9ea46f113b70612125f67
Nicolás Cabello <cabellonic@gmail.com>
2024-02-13 04:43:47 -0300

    feat: :rocket: update package name and configure changesets

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'src/index.ts'),
-			name: '@orion/ui"',
+			name: '@orionpkg/ui"',
 			fileName: format => `index.${format}.js`,
 		},
 		rollupOptions: {
